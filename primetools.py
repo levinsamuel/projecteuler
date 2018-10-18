9b9e3fa45c4832de5b98f8f16719c02389da1f82
Samuel Levin <levinsamuel000@gmail.com>
2018-10-18 19:31:28 +0200

    primetools improvements

--- primetools.py
+++ primetools.py
@@ -8,7 +8,7 @@ import math, logging, sys
 
 if sys.version_info[0] < 3:
     raise Exception("Python 3 or a more recent version is required. Found version is: "
-	+ sys.version)
+    + sys.version)
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
@@ -16,25 +16,38 @@ log = logging.getLogger("primetools")
 log.setLevel(logging.INFO)
 
 
-def findSomePrimes(limit):
+def findSomePrimes(limit = 0, number = 0):
     "Method that returns a map where each key is a prime number. The value of the map will be \
-	initialized to 0. This is for counting prime factors of some other number."
+    initialized to 0. This is for counting prime factors of some other number."
+    
     someprimes={2:0}
-    for i in range(3, limit+1):
-        prime=True
-        for p in someprimes:
-            if i % p == 0:
-                prime=False
-                break
-        if prime:
-            someprimes[i]=0
+    if limit > 0:
+        for i in range(3, limit+1, 2):
+            _addIfPrime(i, someprimes)
+    elif number > 0:
+        i=3
+        while len(someprimes) < number:
+            _addIfPrime(i, someprimes)
+            i += 2
+    else:
+        raise Exception("Either limit or number parameter must be specified and greater than 0.")
+
     return someprimes
+    
+def _addIfPrime(i, someprimes):
+    prime=True
+    for p in someprimes:
+        if i % p == 0:
+            prime=False
+            break
+    if prime:
+        someprimes[i]=0
 
 
 def factor(num):
     
     "Return the prime factorization of a number in the form of a map between the prime key and \
-	the number of those primes in the prime factorization"
+    the number of those primes in the prime factorization"
     root=math.ceil(math.sqrt(num)) + 1
     factors={}
     numagain=num
@@ -55,8 +68,8 @@ def factor(num):
 
 def smallestCommonMultiple(num):
     "Find the smallest number that is divisible by every number from one to num. It does this by \
-	finding all the prime factorizations     of each number and making sure the final map has \
-	exactly as many of each prime factor as the greatest number of any number up to num."
+    finding all the prime factorizations     of each number and making sure the final map has \
+    exactly as many of each prime factor as the greatest number of any number up to num."
     primes = findSomePrimes(num)
     log.debug("primes: {}".format(primes))
     
@@ -71,10 +84,10 @@ def smallestCommonMultiple(num):
 
 if __name__ == '__main__':
 
-	primef=smallestCommonMultiple(20)
-	num=1
-	for k in primef:
-		num *= (k**primef[k])
-		
-	print ("smallest number divisible by 1-20: {}".format(num))
+    primef=smallestCommonMultiple(20)
+    num=1
+    for k in primef:
+        num *= (k**primef[k])
+        
+    print ("smallest number divisible by 1-20: {}".format(num))
 
